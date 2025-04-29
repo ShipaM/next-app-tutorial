5b5755a17b8debc67de6de1794875be1cdf35544
Maksym Shypytsia <maksym.shypytsia@cd.p2h.com>
2025-04-29 20:39:58 +0300

    feat(link component): learn link component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const ProductList = () => {
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>ProductList</h1>
-      <h2>Product1</h2>
-      <h2>Product2</h2>
-      <h2>Product3</h2>
+      <h2>
+        <Link href="/products/1">Product1</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2"> Product2</Link>
+      </h2>
+      <h2>
+        <Link href="/products/3" replace>
+          Product3{" "}
+        </Link>
+      </h2>
     </>
   );
 };
