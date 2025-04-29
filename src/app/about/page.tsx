9bf98235ca7cd2fae3a8c9f6c4f8708ddb230ba7
Maksym Shypytsia <maksym.shypytsia@cd.p2h.com>
2025-04-29 20:04:53 +0300

    feat(routing metadata): learn routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About CodeEvolution",
+};
 const About = () => {
   return <h1>About me</h1>;
 };
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,21 @@
-const ProductDetails = async ({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: Promise<{ productId: string }>;
-}) => {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  return {
+    title: `Product ${id}`,
+  };
+};
+
+const ProductDetails = async ({ params }: Props) => {
   const productId = (await params).productId;
+
   return <h1>Product Details {productId}</h1>;
 };
 
