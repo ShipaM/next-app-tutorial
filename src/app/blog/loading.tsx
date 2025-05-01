d9a14202edb76831137473e3dfac1a20883e7eec
Maksym Shypytsia <maksym.shypytsia@cd.p2h.com>
2025-05-01 11:07:36 +0300

    feat(loading ui): learn loading ui

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = () => {
+  return <div className="p-4 bg-amber-600">Loading...</div>;
+};
+
+export default Loading;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Blog = () => {
+const Blog = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return <h1>My Blog</h1>;
 };
 
