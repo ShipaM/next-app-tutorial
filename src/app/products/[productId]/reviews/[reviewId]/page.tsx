4ffe36b57f033284223ab5bf2210d3e1868ec147
Maksym Shypytsia <maksym.shypytsia@cd.p2h.com>
2025-05-01 12:14:46 +0300

    feat(error handling): learn error handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,21 @@
 import { redirect } from "next/navigation";
 import React from "react";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 const ProductReview = async ({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) => {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
+
   const { reviewId, productId } = await params;
   if (parseInt(reviewId) > 1000) {
     // notFound();
