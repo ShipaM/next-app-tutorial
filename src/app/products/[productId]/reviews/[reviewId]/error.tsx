978229237e7d65537ded159cb16a67584f7c76b6
Maksym Shypytsia <maksym.shypytsia@cd.p2h.com>
2025-05-01 12:32:53 +0300

    feat(recovering from errors): learn recovering from errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,6 +1,30 @@
 "use client";
-const ErrorBoundary = ({ error }: { error: Error }) => {
-  return <div>{error.message}</div>;
+
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+const ErrorBoundary = ({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) => {
+  const router = useRouter();
+
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <div>
+      <p>{error.message}</p>
+      <button className="border border-amber-950 p-4" onClick={reload}>
+        Try again
+      </button>
+    </div>
+  );
 };
 
 export default ErrorBoundary;
